5658d0465a5ea2f7cb8837fb411ffb88a84823b9
Liam Nelski <ljnelski@gmail.com>
2022-10-20 20:54:04 -0400

    Continued work on Lab3 and Re-built Lab1 on laptop

--- LiamNelski_COMP304_Lab3/app/src/main/java/com/example/liamnelski_comp304_lab3/ExerciseOne.java
+++ LiamNelski_COMP304_Lab3/app/src/main/java/com/example/liamnelski_comp304_lab3/ExerciseOne.java
@@ -3,12 +3,24 @@ package com.example.liamnelski_comp304_lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExerciseOne extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_excersise_one);
+        setContentView(R.layout.activity_exercise_one);
+
+        Spinner lineThicknessSpinner = (Spinner) findViewById(R.id.lineThicknessSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.penThickness, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        lineThicknessSpinner.setAdapter(adapter);
     }
 }
--- LiamNelski_COMP304_Lab3/app/src/main/java/com/example/liamnelski_comp304_lab3/MainActivity.java
+++ LiamNelski_COMP304_Lab3/app/src/main/java/com/example/liamnelski_comp304_lab3/MainActivity.java
@@ -5,18 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.exercise1Button).setOnClickListener(new View.OnClickListener() {
+        Button exercise1Button = findViewById(R.id.exercise1Button);
+        exercise1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ExceriseOne.class);
+                Intent intent = new Intent(MainActivity.this, ExerciseOne.class);
                 startActivity(intent);
             }
         });
@@ -24,13 +25,16 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.exercise2Button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openExcersise(ExerciseOne)
+                Intent intent = new Intent(MainActivity.this, ExerciseTwo.class);
+                startActivity(intent);
             }
         });
-        findViewById(R.id.exercise3Button)
-    }
-    private void openExcersise(AppCompatActivity activity) {
-        Intent intent = new Intent(MainActivity.this, activity.class);
-        startActivity(intent);
+        findViewById(R.id.exercise3Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ExerciseThree.class);
+                startActivity(intent);
+            }
+        });
     }
 }
