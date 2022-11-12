bda0c9c0db3fe97ff6d7e194e18771cb2f3e30c2
Liam Nelski <ljnelski@gmail.com>
2022-11-12 13:08:01 -0500

    Update

--- RoomExampleApplication/app/src/main/java/com/example/roomexampleapplication/ReadUserFragment.java
+++ RoomExampleApplication/app/src/main/java/com/example/roomexampleapplication/ReadUserFragment.java
@@ -7,58 +7,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ReadUserFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class ReadUserFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ReadUserFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ReadUserFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ReadUserFragment newInstance(String param1, String param2) {
-        ReadUserFragment fragment = new ReadUserFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private TextView infoTextView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_read_user, container, false);
+        infoTextView = view.findViewById(R.id.infoTextView);
+        List<User> users = MainActivity.myAppDataBase.myDataAccessObject().getUsers();
+
+        String info = "";
+
+        for (User u : users) {
+            int id = u.getId();
+            String name = u.getName();
+            String email = u.getEmail();
+
+            info = "\n\n Id: " + id + "\nName: " + name + "\nEmail: " + email;
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_read_user, container, false);
+        infoTextView.setText(info);
+        return view;
     }
 }
